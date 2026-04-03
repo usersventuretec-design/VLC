5ddf775a96af2e3a69b97b019ce9d7f305b7625c
usersventuretec-design <users.venturetec@gmail.com>
2026-04-03 11:58:05 +0700

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -1,7 +1,10 @@
 export default async function handler(req, res) {
+  // เปลี่ยน URL ตรงนี้เป็นของ Google Script ของคุณ
   const GOOGLE_URL = "https://script.google.com/macros/s/AKfycbwEvei4MHqCJwEnTUYF1aFSXqq0q-em5C5JQUCZJdzoP0ORD-Y6oa93VhN-MYYpKV6u/exec";
+  
   const response = await fetch(GOOGLE_URL);
-  const data = await response.text(); // หรือ .json() ถ้าส่งค่าเป็น json
+  const data = await response.text();
+  
   res.setHeader('Content-Type', 'text/html');
   res.status(200).send(data);
 }
